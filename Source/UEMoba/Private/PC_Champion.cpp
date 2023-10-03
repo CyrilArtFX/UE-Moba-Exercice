d0bee41aee84a31511bb54abe7fa2fba95234776
Cyril Crozat <ccrozat@artfx.fr>
2023-10-03 13:07:20 +0200

    [wip] inputs
    zefionqzrdjivnbeqfcquib

--- Source/UEMoba/Private/PC_Champion.cpp
+++ Source/UEMoba/Private/PC_Champion.cpp
@@ -2,4 +2,31 @@
 
 
 #include "PC_Champion.h"
+#include "BPC_Champion.h"
+#include "InputActionValue.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
+void APC_Champion::BeginPlay()
+{
+	Super::BeginPlay();
+
+	character = Cast<ABPC_Champion>(GetPawn());
+
+	if (UEnhancedInputLocalPlayerSubsystem* input_subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		input_subsystem->AddMappingContext(DefaultMappingContext, 0);
+	}
+}
+
+
+void APC_Champion::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	// Set up action bindings
+	if (UEnhancedInputComponent* enhanced_input_component = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		enhanced_input_component->BindAction(JumpAction, ETriggerEvent::Triggered, character, &ACharacter::Jump);
+	}
+}
--- Source/UEMoba/Public/PC_Champion.h
+++ Source/UEMoba/Public/PC_Champion.h
@@ -13,5 +13,26 @@ UCLASS()
 class UEMOBA_API APC_Champion : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputMappingContext* DefaultMappingContext;
+
+	/** Jump Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* JumpAction;
+
+	/** Move Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* MoveAction;
+
+
+protected:
+	virtual void BeginPlay();
+
+	virtual void SetupInputComponent() override;
+
+
+private:
+	class ABPC_Champion* character;
 };
